830009dbc6b7bf407d2075723461c0ffbbddc246
Mikyle Reyes <reyesmikyle17@gmail.com>
2023-08-27 14:03:04 -0700

    Build debugging tools and add an example

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,7 @@
 "use strict";
 console.log('hello world');
 let thisAge = 20;
+if (thisAge < 50)
+    thisAge += 10;
 console.log(thisAge);
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 console.log('hello world')
 let thisAge: number = 20
+if (thisAge < 50) thisAge += 10
 console.log(thisAge)
