72095e55fb542c36d811c3d1f850b9decc3b64d1
Mikyle Reyes <reyesmikyle17@gmail.com>
2023-08-27 22:37:51 -0700

    Add notes for the union types

--- dist/index.js
+++ dist/index.js
@@ -29,4 +29,22 @@ function calculateTax(income, taxYear = 2022) {
     return income * 1.3;
 }
 calculateTax(10000, 2023);
+let employee = {
+    id: 1,
+    name: '',
+    retire: (date) => {
+        console.log(date);
+    },
+};
+employee.name = 'Mosh';
+function kgToLbs(weight) {
+    if (typeof weight === 'number') {
+        return weight * 2.2;
+    }
+    else {
+        return parseInt(weight) * 2.2;
+    }
+}
+kgToLbs(10);
+kgToLbs('10kg');
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -66,3 +66,16 @@ let employee: Employee = {
 }
 employee.name = 'Mosh'
 // employee.id = 0 // This will error
+
+// Union types
+function kgToLbs(weight: number | string): number {
+  // Narrowing
+  if (typeof weight === 'number') {
+    return weight * 2.2
+  } else {
+    return parseInt(weight) * 2.2
+  }
+}
+
+kgToLbs(10)
+kgToLbs('10kg')
