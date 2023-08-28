a67e38a5bbe0e9b84355f68054568ddbbc4816f0
Mikyle Reyes <reyesmikyle17@gmail.com>
2023-08-27 21:42:33 -0700

    Add notes for enums && functions

--- dist/index.js
+++ dist/index.js
@@ -14,4 +14,13 @@ numbers.forEach(n => n.toString);
 let user = [1, 'Mosh'];
 user[0].toFixed;
 user[1].charAt;
+user.push(1);
+var SizeString;
+(function (SizeString) {
+    SizeString["Small"] = "s";
+    SizeString["Medium"] = "m";
+    SizeString["Large"] = "l";
+})(SizeString || (SizeString = {}));
+let mySize = 2;
+console.log(mySize, 'mySize');
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -19,3 +19,32 @@ let user: [number, string] = [1, 'Mosh']
 user[0].toFixed
 user[1].charAt
 user.push(1)
+
+// Enums
+/// A list of related constants similar to C# or Java
+// const small = 1
+// const medium = 2
+// const large = 3
+
+// PascalCase
+enum SizeString {
+  Small = 's',
+  Medium = 'm',
+  Large = 'l',
+}
+
+const enum Size {
+  Small = 1,
+  Medium, // Defaults to 2 if small = 1 else it will default to 0, 1, 2
+  Large,
+}
+let mySize: Size = Size.Medium
+console.log(mySize, 'mySize')
+
+// Functions
+function calculateTax(income: number, taxYear = 2022): number {
+  if (taxYear < 2022) return income * 1.2
+  return income * 1.3
+}
+
+calculateTax(10_000, 2023)
