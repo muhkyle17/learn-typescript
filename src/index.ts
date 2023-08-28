88e229ee72cc5addf38fb3d10aea7b12fb7d7e7e
Mikyle Reyes <reyesmikyle17@gmail.com>
2023-08-27 19:38:39 -0700

    Notes

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,13 @@ console.log('hello world')
 let thisAge: number = 20
 if (thisAge < 50) thisAge += 10
 console.log(thisAge)
+
+let sales = 123_6345_234234
+let level
+
+function render(document: any) {
+  console.log(document)
+}
+
+let numbers: number[] = []
+numbers.forEach(n => n.toString)
